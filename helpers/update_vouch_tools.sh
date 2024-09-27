e8f387d699e43b5cf79b624dce4aed255da4b2da
JexxaJ <44744647+JexxaJ@users.noreply.github.com>
2024-09-27 12:21:24 +0800

    update

--- helpers/update_vouch_tools.sh
+++ helpers/update_vouch_tools.sh
@@ -4,14 +4,11 @@
 
 
 ### stash and clone
-```sh
 cd $HOME/vouch-val-tools
 git stash
 git pull https://github.com/Vouchrun/vouch-val-tools.git
-```
+
 
 ## Run the GUI menu
-```sh 
 find "$HOME/vouch-val-tools" -type f -name "*.sh" -exec chmod +x {} \;
 ./VouchMenu.sh
-```
